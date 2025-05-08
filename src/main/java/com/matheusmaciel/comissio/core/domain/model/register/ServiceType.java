bd03eac83ce62f930242d8950111fd20f46e03dc
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-05-08 19:06:33 -0300

    feat:Creating ServiceType endpoint

---
+++ src/main/java/com/matheusmaciel/comissio/core/domain/model/register/ServiceType.java
@@ -0,0 +1,44 @@
+package com.matheusmaciel.comissio.core.domain.model.register;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "service_type")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ServiceType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "base_price")
+    private BigDecimal basePrice;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
+
---
+++ src/main/java/com/matheusmaciel/comissio/core/domain/repository/ServiceTypeRepository.java
@@ -0,0 +1,9 @@
+package com.matheusmaciel.comissio.core.domain.repository;
+
+import com.matheusmaciel.comissio.core.domain.model.register.ServiceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ServiceTypeRepository extends JpaRepository<ServiceType, UUID> {
+}
