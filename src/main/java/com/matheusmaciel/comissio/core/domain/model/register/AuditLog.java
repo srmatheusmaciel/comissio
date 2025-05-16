b8725ad1dd8c71fad5e23bbc7ad6449a23ca7e36
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-05-16 17:38:42 -0300

    fix bug in audit log and user models
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

--- src/main/java/com/matheusmaciel/comissio/core/domain/model/register/AuditLog.java
+++ src/main/java/com/matheusmaciel/comissio/core/domain/model/register/AuditLog.java
@@ -12,13 +12,15 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "audit_log")
+@Table(name = "audit_logs")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class AuditLog {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @ManyToOne
