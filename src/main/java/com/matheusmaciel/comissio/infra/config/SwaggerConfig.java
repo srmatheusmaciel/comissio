736c019c53918bb79f0767436381dafc24b174b0
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-05-17 19:20:43 -0300

    updated SwaggerConfig
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

--- src/main/java/com/matheusmaciel/comissio/infra/config/SwaggerConfig.java
+++ src/main/java/com/matheusmaciel/comissio/infra/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.matheusmaciel.comissio.infra.config;
 
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
