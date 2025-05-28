9ff787a68bb2ec3e8c563d822f9a9ec3322f8493
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-05-28 19:18:09 -0300

    fix: I fixed the bug that did not bring more than 1 commission per employee
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/comissio/core/domain/repository/EmployeeComissionRepository.java
+++ src/main/java/com/matheusmaciel/comissio/core/domain/repository/EmployeeComissionRepository.java
@@ -3,13 +3,14 @@ package com.matheusmaciel.comissio.core.domain.repository;
 import com.matheusmaciel.comissio.core.domain.model.register.EmployeeComission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface EmployeeComissionRepository extends JpaRepository<EmployeeComission, UUID> {
     Optional<EmployeeComission> findByEmployeeIdAndServiceTypeId(UUID employeeId, UUID serviceTypeId);
 
-    Optional<EmployeeComission> findByEmployeeId(UUID employeeId);
+    List<EmployeeComission> findByEmployeeId(UUID employeeId);
 
     Optional<EmployeeComission> findByServiceTypeId(UUID serviceTypeId);
 }
