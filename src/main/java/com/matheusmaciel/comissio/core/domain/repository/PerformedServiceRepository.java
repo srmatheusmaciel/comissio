f98873bcdd5fe81201f4c2b29ef0045316f44221
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-05-31 20:33:19 -0300

    Created report endpoint
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/comissio/core/domain/repository/PerformedServiceRepository.java
+++ src/main/java/com/matheusmaciel/comissio/core/domain/repository/PerformedServiceRepository.java
@@ -3,12 +3,13 @@ package com.matheusmaciel.comissio.core.domain.repository;
 import com.matheusmaciel.comissio.core.domain.model.register.PerformedService;
 import com.matheusmaciel.comissio.core.domain.model.register.ServiceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-public interface PerformedServiceRepository extends JpaRepository<PerformedService, UUID> {
+public interface PerformedServiceRepository extends JpaRepository<PerformedService, UUID>, JpaSpecificationExecutor<PerformedService> {
     List<PerformedService> findByEmployee_IdAndStatusAndServiceDateLessThanEqual(
             UUID employeeId,
             ServiceStatus status,
